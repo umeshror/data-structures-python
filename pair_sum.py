3d484322f3df62c3fdc22d1c813095de6871dabe
umeshror <sarukumesh@gmail.com>
2019-10-29 11:49:09 +0530

    added pair-sum

--- pair_sum.py
+++ pair_sum.py
@@ -12,22 +12,21 @@ would return 2 pairs:
 
 """
 
-# solution 1
+# solution 1 : O(nlogn)
 
 def pair_sum_sol1(arr, k):
     if len(arr) < 2:
         return
     found = []
-    for outer_ind, element in enumerate(arr, 1):
-        for item in arr[outer_ind:]:
+    for ind, element in enumerate(arr, 1):
+        for item in arr[ind:]:
             if element + item is k:
                 found.append((element, item))
     return len(found)
 # output 2
 # pair_sum_sol1([1, 3, 2, 2], 4)
 
-# solution 2
-
+# solution 2 : O(n)
 def pair_sum_sol2(arr, k):
     counter = 0
     lookup = set()
