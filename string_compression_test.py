7b1a728c231ce7825e6c3886c5179a479dcf5cb7
umeshror <sarukumesh@gmail.com>
2019-10-29 19:12:17 +0530

    added string_compression

---
+++ string_compression_test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from string_compression import compress
+
+
+class TestCompress(TestCase):
+    def test_compress(self):
+        self.assertEqual(compress(''), '')
+        self.assertEqual(compress('AABBCC'), 'A2B2C2')
+        self.assertEqual(compress('AAABCCDDDDD'), 'A3B1C2D5')
+        self.assertEqual(compress('A'), 'A1')
+        self.assertEqual(compress('AAAaaa'), 'A3a3')
