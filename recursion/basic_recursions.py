7335d1b2d47cb63f8044731b275daf620d8785ec
umeshror <sarukumesh@gmail.com>
2019-10-30 23:58:45 +0530

    sum_func added

--- recursion/basic_recursions.py
+++ recursion/basic_recursions.py
@@ -19,3 +19,22 @@ def find_sum(n):
 
 
 print(find_sum(5))
+
+"""
+Given an integer, create a function which returns the sum of all the individual
+ digits in that integer. For example: if n = 4321, return 4+3+2+1
+
+"""
+
+
+def sum_func(n):
+    # Base case
+    if len(str(n)) == 1:
+        return n
+
+    # Recursion
+    else:
+        return n % 10 + sum_func(n / 10)
+
+
+print(sum_func(4321))
