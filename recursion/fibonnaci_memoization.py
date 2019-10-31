6cd93c3a28b1d3980c54ce235c2ebcc506dcf00b
umeshror <sarukumesh@gmail.com>
2019-10-31 23:20:51 +0530

    fib_recursion added

--- recursion/fibonnaci_memoization.py
+++ recursion/fibonnaci_memoization.py
@@ -11,6 +11,18 @@ Iteratively
 
 
 def fib_rec(n):
+    """
+
+    fib_rec(n)  = fib_rec(n - 1) + fib_rec(n - 2)
+    fib_rec(2)  = fib_rec(1) + fib_rec(0)
+    fib_rec(3)  = fib_rec(2) + fib_rec(1)
+    fib_rec(5)  = fib_rec(4) + fib_rec(3)
+                = fib_rec(3) + fib_rec(2) + fib_rec(2) + fib_rec(1)
+                = fib_rec(2) + fib_rec(1) + fib_rec(2) + fib_rec(2) + fib_rec(1)
+
+    2 =  1 + 1
+    3 =  2 + 1
+    """
     # Base Case
     if n == 0 or n == 1:
         return n
