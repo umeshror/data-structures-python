3791fa50b89e5ffb311b6b912da55919b3ee8222
umeshror <sarukumesh@gmail.com>
2019-11-01 15:27:28 +0530

    fixed naming

--- trees/tree_list_represntation.py
+++ trees/tree_list_represntation.py
@@ -6,38 +6,38 @@ class BinaryTree(object):
         """
         self.root = [root, [], []]
 
-    def insertLeft(self, newBranch):
+    def insert_left(self, new_branch):
         t = self.root.pop(1)
         if len(t) > 1:
-            self.root.insert(1, [newBranch, t, []])
+            self.root.insert(1, [new_branch, t, []])
         else:
-            self.root.insert(1, [newBranch, [], []])
+            self.root.insert(1, [new_branch, [], []])
         return self.root
 
-    def insertRight(self, newBranch):
+    def insert_right(self, new_branch):
         t = self.root.pop(2)
         if len(t) > 1:
-            self.root.insert(2, [newBranch, [], t])
+            self.root.insert(2, [new_branch, [], t])
         else:
-            self.root.insert(2, [newBranch, [], []])
+            self.root.insert(2, [new_branch, [], []])
         return self.root
 
-    def getRootVal(self):
+    def get_root_val(self):
         return self.root[0]
 
-    def setRootVal(self, newVal):
+    def set_root_val(self, newVal):
         self.root[0] = newVal
 
-    def getLeftChild(self):
+    def get_left_child(self):
         return self.root[1]
 
-    def getRightChild(self):
+    def get_right_child(self):
         return self.root[2]
 
 
 r = BinaryTree(3)
 
-print r.insertLeft(2)
-print r.insertLeft(1)
-print r.insertRight(4)
-print r.insertRight(5)
+print r.insert_left(2)
+print r.insert_left(1)
+print r.insert_right(4)
+print r.insert_right(5)
