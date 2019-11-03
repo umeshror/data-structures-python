ec5497b51c0b8e3f0ba1d0e827e123e4281dc5ea
umeshror <sarukumesh@gmail.com>
2019-11-03 23:42:48 +0530

    binary_search tests added

---
+++ search_sort/binary_search_test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from search_sort.binary_search import binary_search
+
+
+class BinnarySearchTest(TestCase):
+    def test(self):
+        self.assertTrue(binary_search([1, 2, 3, 4, 5, 6], 3))
+        self.assertTrue(binary_search([1, 2, 3, 4, 5, 6], 1))
+        self.assertFalse(binary_search([1, 2, 3, 4, 5, 6], 44))
