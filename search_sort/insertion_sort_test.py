9c48acc896c0b08cf2e5079079e0d259022002a5
umeshror <sarukumesh@gmail.com>
2019-11-09 23:53:54 +0530

    insertion_sort_sol2 tests added

--- search_sort/insertion_sort_test.py
+++ search_sort/insertion_sort_test.py
@@ -1,14 +1,22 @@
 from unittest import TestCase
 
-from search_sort.insertion_sort import insertion_sort_sol1
+from search_sort.insertion_sort import insertion_sort_sol1, insertion_sort_sol2
 
 
 class SelectionSortTest(TestCase):
-    def test(self):
+    def test_sol1(self):
         self.assertEqual(insertion_sort_sol1([-12, 431, 12, 5]), [-12, 5, 12, 431])
         self.assertEqual(insertion_sort_sol1([-1, -2, -12, -4]), [-12, -4, -2, -1])
         self.assertEqual(insertion_sort_sol1([-12, 431, 12, 5]), [-12, 5, 12,
                                                                 431])
         self.assertEqual(insertion_sort_sol1([-1, -2, -12, -4]), [-12, -4, -2, -1])
+
+
+    def test_sol2(self):
+        self.assertEqual(insertion_sort_sol2([-12, 431, 12, 5]), [-12, 5, 12, 431])
+        self.assertEqual(insertion_sort_sol2([-1, -2, -12, -4]), [-12, -4, -2, -1])
+        self.assertEqual(insertion_sort_sol2([-12, 431, 12, 5]), [-12, 5, 12,
+                                                                431])
+        self.assertEqual(insertion_sort_sol2([-1, -2, -12, -4]), [-12, -4, -2, -1])
 
 
