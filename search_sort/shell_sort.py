4b4a4cb821d4bf5ee6acdca233d9612279cce919
umeshror <sarukumesh@gmail.com>
2019-11-10 09:28:04 +0530

    shell_sort added

--- search_sort/shell_sort.py
+++ search_sort/shell_sort.py
@@ -3,23 +3,13 @@ def shell_sort(arr):
     n = len(arr)
     gap = n / 2
 
-    # Do a gapped insertion sort for this gap size.
-    # The first gap elements a[0..gap-1] are already in gapped
-    # order keep adding one more element until the entire array
-    # is gap sorted
     while gap > 0:
 
         for i in range(gap, n):
 
-            # add a[i] to the elements that have been gap sorted
-            # save a[i] in temp and make a hole at position i
             temp = arr[i]
-
-            # shift earlier gap-sorted elements up until the correct
-            # location for a[i] is found
             j = i
-            s = gap
-            x = arr[j - gap]
+
             while j >= gap and arr[j - gap] > temp:
                 arr[j] = arr[j - gap]
                 j -= gap
