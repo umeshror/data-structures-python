7978dffcc898ba170ea8f78e44e7a2d46189a2ea
umeshror <sarukumesh@gmail.com>
2019-11-12 09:31:36 +0530

    merge_sort added

--- search_sort/merge_sort.py
+++ search_sort/merge_sort.py
@@ -1,39 +1,44 @@
 def merge_sort(arr):
-    if len(arr)>1:
-        middle_ind = len(arr) / 2
+    if len(arr) == 1:
+        # reached at the last element
+        return arr
 
-        left_half = arr[:middle_ind]
-        right_half = arr[middle_ind:]
+    middle_ind = len(arr) / 2
 
-        sorted_left_half = merge_sort(left_half)
-        sorted_right_half = merge_sort(right_half)
+    left_half = arr[:middle_ind]
+    right_half = arr[middle_ind:]
 
-        i = 0
-        j = 0
-        k = 0
+    sorted_left_half = merge_sort(left_half)
+    sorted_right_half = merge_sort(right_half)
 
-        while i < len(sorted_left_half) and j < len(sorted_right_half):
-            if sorted_left_half[i] < sorted_right_half[j]:
-                arr[k] = sorted_left_half[i]
-                i += 1
+    i = 0
+    j = 0
+    k = 0
 
-            else:
-                arr[k] = sorted_right_half[j]
-                j += 1
-            k += 1
-
-        while i < len(sorted_left_half):
+    while i < len(sorted_left_half) and j < len(sorted_right_half):
+        if sorted_left_half[i] < sorted_right_half[j]:
             arr[k] = sorted_left_half[i]
             i += 1
-            k += 1
 
-        while j < len(sorted_right_half):
+        else:
             arr[k] = sorted_right_half[j]
             j += 1
-            k += 1
+        k += 1
 
-        print("Sorted : {}, {} to {}".format(sorted_left_half, sorted_right_half, arr))
+    while i < len(sorted_left_half):
+        arr[k] = sorted_left_half[i]
+        i += 1
+        k += 1
+
+    while j < len(sorted_right_half):
+        arr[k] = sorted_right_half[j]
+        j += 1
+        k += 1
+
+    print("Sorted : {}, {} to {}".format(sorted_left_half, sorted_right_half, arr))
     return arr
 
 arr = [11,2,5,4,7,6,8,1,23]
 print merge_sort(arr)
+
+
