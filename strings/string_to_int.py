835f38d921e1e02e5f3877dc51a6e76322778295
umeshsaruk <sarukumesh@gmail.com>
2019-11-19 19:37:32 +0530

    string to int  added

---
+++ strings/string_to_int.py
@@ -0,0 +1,46 @@
+"""
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found.
+ Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as
+  possible, and interprets them as a numerical value.
+
+Input: "42"
+Output: 42
+
+Input: "   -42"
+Output: -42
+
+Input: "4193 with words"
+Output: 4193
+
+Input: "words and 987"
+Output: 0
+
+Input: "-91283472332"
+Output: -2147483648
+
+"""
+def atoi(strng):
+    strng = strng.strip()
+    if len(strng) == 0:
+        return 0
+    out = "0"
+    i = 0
+    if strng[0] in "+-":
+        out = strng[0]
+        i = 1
+    for char in strng[i:]:
+        if char.isdigit():
+            out += char
+        else:
+            break
+    # if out is + or -
+    if len(out) == 1:
+        return 0
+    out = int(out)
+    if (out.bit_length() >= 32):
+        return (2 ** 31 - 1) if out > 0 else -2 ** 31
+    return out
+print(atoi( "   -42l knj;nuvk"))
+print(atoi("+"))
+print(atoi(""))
+print(atoi("+-"))
