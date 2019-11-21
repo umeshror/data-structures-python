a156e2a61669b28cc5341edd090bdfa85140b5e1
umeshsaruk <sarukumesh@gmail.com>
2019-11-21 23:21:10 +0530

    int to roman tests added

---
+++ maths/tests/integer_to_roman_test.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from maths.integer_to_roman import integer_to_roman, integer_to_roman_sol2
+
+
+class IntegerToRomanTest(TestCase):
+    def test_sol1(self):
+        self.assertEqual(integer_to_roman(3), "III")
+        self.assertEqual(integer_to_roman(4), "IV")
+        self.assertEqual(integer_to_roman(8), "VIII")
+        self.assertEqual(integer_to_roman(58), "LVIII")
+        self.assertEqual(integer_to_roman(588), "DLXXXVIII")
+        self.assertEqual(integer_to_roman(3999), "MMMCMXCIX")
+
+    def test_sol2(self):
+        self.assertEqual(integer_to_roman_sol2(3), "III")
+        self.assertEqual(integer_to_roman_sol2(4), "IV")
+        self.assertEqual(integer_to_roman_sol2(8), "VIII")
+        self.assertEqual(integer_to_roman_sol2(58), "LVIII")
+        self.assertEqual(integer_to_roman_sol2(588), "DLXXXVIII")
+        self.assertEqual(integer_to_roman_sol2(3999), "MMMCMXCIX")
