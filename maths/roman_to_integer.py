e1235557af957304670621d58aa0b899142a7cac
umeshsaruk <sarukumesh@gmail.com>
2019-11-24 08:34:54 +0530

    roman_to_int added

--- maths/roman_to_integer.py
+++ maths/roman_to_integer.py
@@ -14,18 +14,31 @@ Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 """
-def roman_to_int(s):
-    symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    last_value = result = symbols[s[-1]]
 
-    for i in reversed(s[:-1]):
-        value = symbols[i]
-        if value < last_value:
-            result -= value
+
+def roman_to_int(roman_num):
+    """
+    Get the last char value.
+    Compare this value to second last.
+    IF greater current_val >=  last_value then add to out
+    else substract
+    :param roman_num:  Roman str e.g.MCMXCIV
+    :return: INT 194
+    """
+    symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    last_value = symbols[roman_num[-1]]
+    int_num = last_value
+
+    for char in reversed(roman_num[:-1]):
+        current_val = symbols[char]
+        if current_val >= last_value:
+            int_num += current_val
         else:
-            result += value
-        last_value = value
-    return result
+            int_num -= current_val
+        last_value = current_val
+
+    return int_num
 
 
 print(roman_to_int("III"))
