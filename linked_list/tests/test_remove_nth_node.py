07364ba20132275ca4f3a9f2c5a75f1028bf17da
umeshsaruk <sarukumesh@gmail.com>
2019-12-04 22:04:47 +0530

    TestRemoveNthNode added

---
+++ linked_list/tests/test_remove_nth_node.py
@@ -0,0 +1,20 @@
+import unittest
+
+from linked_list.remove_nth_node import remove_nth_frm_end, generate_nodes, traverse_list
+
+
+class TestRemoveNthNode(unittest.TestCase):
+    def test_1(self):
+        arr1 = generate_nodes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        out = remove_nth_frm_end(arr1, 4)
+        self.assertEqual(traverse_list(out), [1, 2, 3, 4, 5, 6, 8, 9, 10])
+
+    def test_2(self):
+        arr1 = generate_nodes([1, 2, 3])
+        out = remove_nth_frm_end(arr1, 1)
+        self.assertEqual(traverse_list(out), [1, 2])
+
+    def test_3(self):
+        arr1 = generate_nodes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        out = remove_nth_frm_end(arr1, 7)
+        self.assertEqual(traverse_list(out), [1, 2, 3, 5, 6, 7, 8, 9, 10])
