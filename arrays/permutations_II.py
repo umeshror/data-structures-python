e56d0464fe26060ec744ae98b8905273d933e33d
umeshsaruk <sarukumesh@gmail.com>
2019-12-18 13:51:57 +0530

    unique_permutations added

---
+++ arrays/permutations_II.py
@@ -0,0 +1,39 @@
+"""
+Given a collection of numbers that might contain duplicates,
+ return all possible unique permutations.
+
+Example:
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+"""
+
+
+def find_unique_permutations(arr):
+    out = []
+    arr.sort()
+    dfs(arr, [], out)
+    return out
+
+
+def dfs(arr, path, out):
+    """
+    :param arr: List of integers
+    :param path: Path reached till now
+    :param out: Response
+    """
+    if not arr:
+        out.append(path)
+        return
+
+    for i in range(len(arr)):
+        # skip to next element as its already handled
+        if len(arr) > 1 and arr[i] == arr[i-1]:
+            continue
+        dfs(arr[:i] + arr[i + 1:], path + [arr[i]], out)
+print(find_unique_permutations([1, 1, 2]))
