c16313c91055711e5e12a346a1018ae1e012679c
umesh <sarukumesh@gmail.com>
2020-01-13 23:13:07 +0530

    next_permutation_sol2 added

--- arrays/next_permutation.py
+++ arrays/next_permutation.py
@@ -46,3 +46,33 @@ def next_permutation(nums):
 print(next_permutation([1, 2, 3]))
 print(next_permutation([2, 1, 3]))
 print(next_permutation([3, 2, 1]))
+
+
+def next_permutation_sol2(nums):
+    """
+    :type nums: List[int]
+    """
+    # Use two-pointers: two pointers start from back
+    # first pointer j stop at descending point
+    # second pointer i stop at value > nums[j]
+    # swap and sort rest
+    if not nums:
+        return None
+    i = len(nums) - 1
+    j = -1  # j is set to -1 for case `4321`, so need to reverse all in following step
+    while i > 0:
+        if nums[i - 1] < nums[i]:  # first one violates the trend
+            j = i - 1
+            break
+        i -= 1
+    for i in range(len(nums) - 1, -1, -1):
+        if nums[i] > nums[j]:  #
+            nums[i], nums[j] = nums[j], nums[i]  # swap position
+            nums[j + 1:] = sorted(nums[j + 1:])  # sort rest
+            break
+    return nums
+
+
+print(next_permutation_sol2([1, 2, 3]))
+print(next_permutation_sol2([2, 1, 3]))
+print(next_permutation_sol2([3, 2, 1]))
