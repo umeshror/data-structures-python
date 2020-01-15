594ee64e2c78112caa37928dad52f2de4a9c415e
umesh <sarukumesh@gmail.com>
2020-01-15 23:21:29 +0530

    search_insert added

---
+++ binary_search/search_insert_position.py
@@ -0,0 +1,45 @@
+"""
+Given a sorted array and a target value, return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Input: [1,3,5,6], 5
+Output: 2
+
+Input: [1,3,5,6], 2
+Output: 1
+
+Input: [1,3,5,6], 7
+Output: 4
+
+Input: [1,3,5,6], 0
+Output: 0
+"""
+
+
+def search_insert(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    l = 0
+    r = len(nums) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] < target:
+            l = mid + 1
+        else:
+            if nums[mid] == target and nums[mid - 1] != target:
+                return mid
+            else:
+                r = mid - 1
+
+    return l
+
+
+print(search_insert([1, 3, 5, 6], 5))
+print(search_insert([1, 3, 5, 6], 2))
+print(search_insert([1, 3, 5, 6], 7))
+print(search_insert([1, 3, 5, 6], 0))
